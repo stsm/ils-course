f4987d0e93948ae29a6a5dc7f54743ef77c01419
Stephan Schmidt <rix82@gmx.de>
2011-12-12 23:26:19 +0100

    Signed-off-by: Stephan Schmidt <rix82@gmx.de>

---
+++ cpbs12b/cpbs12b_ea_2.cpp
@@ -0,0 +1,24 @@
+try
+{
+	//try something...
+}
+catch (FormatException^)
+{
+	//format exception
+}
+catch (DivideByZeroException^)
+{
+	//divide by zero
+}
+catch (StackOverflowException^)
+{
+	//stack overflow
+}
+catch (IO::FileNotFoundException^)
+{
+	//file not found
+}
+catch(...)
+{
+	//all other exceptions
+}
